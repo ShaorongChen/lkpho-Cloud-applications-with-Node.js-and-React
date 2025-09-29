0494eaf5d096e441dee8cfd255fe7ce1fff2965a
Shaorong Chen <csrnfs@gmail.com>
2025-09-29 00:40:27 -0400

    Completed Express Server Lab

--- CD220Labs/expressjs/expressWithAuthentication.js
+++ CD220Labs/expressjs/expressWithAuthentication.js
@@ -48,6 +48,7 @@ app.use("/auth", function auth(req, res, next) {
 app.post("/login", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
+  console.log(username+" "+password);
 
   if (!username || !password) {
     return res.status(404).json({ message: "Error logging in" });
